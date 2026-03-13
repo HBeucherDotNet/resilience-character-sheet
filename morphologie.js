af19a394c873621d0ee763ef8ef9ce951dca83fb
HBeucher <Hadrien.beucher@gmail.com>
2026-03-13 17:28:39 +0100

    Base pour les morphologies

---
+++ morphologie.js
@@ -0,0 +1,107 @@
+export const morphologies = {
+	articule: {
+		nom: "Armement articulé ❄️",
+		categorie: "armement",
+		description: "Un Armement Articulé s’utilise pour fuir, entraver, étrangler, manipuler, renvoyer l’attaque de l’adversaire ou le mettre face à ses contradictions."
+	},
+	distance: {
+		nom: "Armement à Distance 🌱",
+		categorie: "armement",
+		description: "Un Armement à Distance s’utilise pour feinter, percer, désarmer, rester hors de danger, embrouiller ou distraire."
+	},
+	court: {
+		nom: "Armement Court ☀️",
+		categorie: "armement",
+		description: "Un Armement Court s’utilise pour charger, mettre en fuite, trancher, submerger, intimider et attaquer sur les émotions."
+	},
+	long: {
+		nom: "Armement Long 🍁",
+		categorie: "armement",
+		description: "Un Armement Long s’utilise pour tenir la ligne, épuiser l’adversaire, assommer, renverser, faire preuve de logique et contrôler l’espace."
+	},
+	crete: {
+		nom: "Crête",
+		categorie: "armement",
+		description: "Une Crête s’utilise en Conflit. C’est une Morphologie de la Saison Appelée. Chaque utilisation d’une Crête coûte 1 point de Souffle."
+	},
+	mucus: {
+		nom: "Mucus ❄️",
+		categorie: "cuirasse",
+		description: "Une Cuirasse réduit les Dégâts subit par le personnage lorsqu’il est victime d’une Agression de la Saison associée à l’Armure."
+	},
+	urticant: {
+		nom: "Urticant 🌱",
+		categorie: "cuirasse",
+		description: "Une Cuirasse réduit les Dégâts subit par le personnage lorsqu’il est victime d’une Agression de la Saison associée à l’Armure."
+	},
+	bourre: {
+		nom: "Bourre ☀️",
+		categorie: "cuirasse",
+		description: "Une Cuirasse réduit les Dégâts subit par le personnage lorsqu’il est victime d’une Agression de la Saison associée à l’Armure."
+	},
+	carapace: {
+		nom: "Carapace 🍁",
+		categorie: "cuirasse",
+		description: "Une Cuirasse réduit les Dégâts subit par le personnage lorsqu’il est victime d’une Agression de la Saison associée à l’Armure."
+	},	
+	ventouses: {
+		nom: "Ventouses ❄️",
+		categorie: "mains",
+		description: "Appel impliquant des animaux, de la chair, des poils, de la fourrure, de la peau, de la chitine ou des plumes."
+	},
+	vibrisses: {
+		nom: "Vibrisses 🌱",
+		categorie: "mains",
+		description: "Appel impliquant des plantes, champignons, des feuilles, de l’écorce ou du bois."
+	},
+	crochets: {
+		nom: "Crochets ☀️",
+		categorie: "mains",
+		description: "Appel impliquant du feu, des résines, des huiles et autres extraits (sucs, sève, sang, lymphe, venin…)"
+	},
+	pinces: {
+		nom: "Pinces 🍁",
+		categorie: "mains",
+		description: "Appel impliquant des matériaux inertes comme la pierre, le silex, l’argile, le sel, l’os ou les coquilles."
+	},
+	membrane: {
+		nom: "Membrane ❄️",
+		categorie: "peau",
+		description: "Une Membrane s’utilise pour les actions physiques."
+	},
+	camouflage: {
+		nom: "Camouflage 🌱",
+		categorie: "peau",
+		description: "Un Camouflage s’utilise pour les actions de perception."
+	},
+	aposematisme: {
+		nom: "Aposématisme ☀️",
+		categorie: "peau",
+		description: "Un Aposématisme s’utilise pour les actions sociales."
+	},
+	ecailles: {
+		nom: "Écailles 🍁",
+		categorie: "peau",
+		description: "Des Écailles s’utilisent pour les actions mentales ou manuelles."
+	},
+	comestible: {
+		nom: "Comestible ❄️",
+		categorie: "matiere",
+		description: "Fruit, graine, feuilles, miellat, lait, nectar…"
+	},
+	secretion: {
+		nom: "Sécrétion 🌱",
+		categorie: "matiere",
+		description: "Latex, laine, cire, soie, sève, huile…"
+	},
+	ornement: {
+		nom: "Ornement ☀️",	
+		categorie: "matiere",
+		description: "Crinière, fleur, ambre, plume, pigment, nacre…"
+	},	
+	ossature: {
+		nom: "Ossature 🍁",
+		categorie: "matiere",
+		description: "Bois, os, chitine, coquillage, écaille, pierre…"
+	}
+};
