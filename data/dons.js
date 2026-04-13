4ebece01f586e09a3638b9185e301974f42d00b1
HBeucher <Hadrien.beucher@gmail.com>
2026-04-13 14:33:31 +0200

    Adaptation des dons

--- data/dons.js
+++ data/dons.js
@@ -1,27 +1,27 @@
 export const dons = {
 	adrenaline: {
-		nom: "Adrénaline ☀️",
+		nom: "Adrénaline",
 		categorie: "famille",
-		saison: "été",
+		saison: "ete",
 		summary: "S’utilise lors des Conflits ou lorsque le personnage est en danger immédiat",
 		description: "Les personnages ayant ce Don sont capables de dépasser leurs limites physiques pendant un bref laps de temps. Juste assez pour se mettre hors de danger par la fuite ou le combat. Ce Don s’utilise lors des Conflits ou lorsque le personnage est en danger immédiat."
 	},
 	symbiose: {
-		nom: "Symbiose 🌱",
+		nom: "Symbiose",
 		categorie: "famille",
 		saison: "printemps",
 		summary: "S’utilise quand le personnage reçoit de l’Aide, bénéficie de ou fait un Support ou une Tactique",
 		description: "Les personnages ayant ce Don savent comment faire en sorte que le tout soit supérieur à la somme de parties. Ils tirent une meilleure efficacité de l’association de qualités différentes. Ce Don s’utilise quand le personnage reçoit de l’Aide, bénéficie de ou fait un Support ou une Tactique."
 	},
 	exosquelette: {
-		nom: "Exosquelette 🍁",
+		nom: "Exosquelette",
 		categorie: "famille",
 		saison: "automne",
 		summary: "Épreuves de force, d'endurance ou de récolte. La carapace augmente puissance physique et résistance",
 		description: "Les personnages ayant ce Don ont le corps couvert d’une carapace décuplant sa puissance et garnie d’excroissances utiles. Ce Don s’utilise pour les épreuves de force, d'endurance et la récolte de ressources."
 	},
 	mimetisme: {
-		nom: "Mimétisme ❄️",
+		nom: "Mimétisme",
 		categorie: "famille",
 		saison: "hiver",
 		summary: "Épreuves d’agilité, la discrétion, le déguisement et l’imitation",
@@ -30,120 +30,140 @@ export const dons = {
 	albinos: {
 		nom: "Albinos",
 		categorie: "commun",
+		saison: null,
 		summary: "Détruire ou forger de la Pourprine; pour les Attaques contre les Vecteurs de Rupture",
 		description: "Le personnage est albinos. Pour une raison inconnue, cela semble lui donner un certain ascendant sur la Rupture. Ce Don s’utilise pour détruire ou forger de la Pourprine et pour les Attaques contre les Vecteurs de Rupture."
 	},
 	hibernation: {
 		nom: "Hibernation",
 		categorie: "commun",
+		saison: null,
 		summary: "S’utilise lors des Intempéries et lorsque le personnage est dans un milieu inhospitalier ou un milieu hostile",
 		description: "Le personnage sait ralentir son métabolisme pour supporter des conditions difficiles voire létales. Ce Don s'utilise lors des Intempéries et lorsque le personnage est dans un milieu inhospitalier ou un milieu hostile. Ce Don est uniquement utile dans le Monde d’origine du personnage (Matériel, Altjira, Mictlan…)."
 	},
 	regeneration: {
 		nom: "Régénération",
 		categorie: "commun",
+		saison: null,
 		summary: "S’utilise quand le personnage bénéficie de Résilience, récupère de blessures graves sans Appel",
-		description: "Le personnage récupère des blessures les plus graves. Ce Don s’utilise quand le personnage bénéficie de la Résilience. Ce Don ne nécessite donc pas d’Appel. Le sang du personnage (ou équivalent) est légèrement brillant."
+		description: "Le personnage récupère des blessures les plus graves. Ce Don s’utilise quand le personnage bénéficie d’un Repos : il fait alors un Appel de Soins sur lui-même, bénéficiant du Don, au lieu d’appliquer le Repos (voir « Santé et Agressions »). Ce Don peut aussi servir quand le personnage fait un Appel de Soins pour quelqu’un d’autre : il lui fait profiter de ses composés régénérateurs. Le sang du personnage (ou équivalent) est légèrement brillant."
 	},
 	transe: {
 		nom: "Transe",
 		categorie: "commun",
+		saison: null,
 		summary: "Rester lucide/concentré ou agir hors de son monde d'origine, contrôle ses rêves et agit dans les mondes spirituels",
-		description: "Le personnage contrôle ses rêves et arpente consciemment les Mondes Spirituels. Ce Don s’utilise quand le personnage doit rester lucide, concentré ou maitre de lui-même, et quand il agit dans un Monde différent de son Monde d’origine (Matériel, Altjira, Mictlan, Psychée…). Les yeux du personnage (ou leur équivalent) sont ouverts en permanence et ne clignent jamais."
+		description: "Le personnage contrôle ses rêves et arpente consciemment les Mondes Spirituels. Ce Don s’utilise quand le personnage doit rester lucide, concentré ou maitre de lui-même, et quand il agit dans un Monde différent de son Monde d’origine (Matériel, Altjira, Mictlan, Psychée…). Les yeux du personnage (ou leur équivalent) sont ouverts en permanence et ne clignent jamais. NB : un personnage avec ce Don est, ou a été, certainement en contact avec la Toile (voir « Livre Univers »). Il est important de discuter entre MJ et joueuse de ce que son personnage sait, ou pas, et de son potentiel rôle dans cette organisation."
 	},
 	amphibie: {
-		nom: "Amphibie ❄️",
+		nom: "Amphibie",
 		categorie: "mobilite",
+		saison: "hiver",
 		summary: "Nager, retenir son souffle et agir sans respirer",
 		description: "Le personnage est à l’aise dans l’eau et retient facilement son souffle. Ce Don s’utilise pour nager, retenir son souffle et agir sans respirer."
 	},
 	bond: {
-		nom: "Bond ❄️",
+		nom: "Bond",
 		categorie: "mobilite",
-		summary: "Sauts, acrobaties et réceptions périlleuses; peut voler pour 2 points de Souffle par usage",
-		description: "À l’aide d’ailes, de pattes puissantes ou d’une sorte de parachute, le personnage peut effectuer des sauts gigantesques et se prémunir des chutes. Ce Don s’utilise pour les sauts, acrobaties et réceptions périlleuses. NB : ce Don peut aussi servir à voler si le personnage a des ailes. Cependant, voler demande de dépenser 2 points de Souffle au lieu d’un seul pour conserver le Don."
+		saison: "hiver",
+		summary: "Sauts, acrobaties et réceptions périlleuses",
+		description: "À l’aide d’ailes, de pattes puissantes ou d’une sorte de parachute, le personnage peut effectuer des sauts gigantesques et se prémunir des chutes. Ce Don s’utilise pour les sauts, acrobaties et réceptions périlleuses."
 	},
 	grimper: {
-		nom: "Grimper ❄️",
+		nom: "Grimper",
 		categorie: "mobilite",
+		saison: "hiver",
 		summary: "Escalader, se suspendre et éviter de tomber",
 		description: "Les membres du personnages sont particulièrement adhérents à toutes les surfaces. Ce Don s’utilise pour escalader, se suspendre et éviter de tomber."
 	},
 	terrier: {
-		nom: "Terrier ❄️",
+		nom: "Terrier",
 		categorie: "mobilite",
-		summary: "Creuser des tunnels, la terre ou la roche",
-		description: "Avec ses racines, ses pattes ou son corps fouisseur, le personnage peut creuser la terre, excaver et fragmenter les roches. Ce Don s’utilise pour creuser des tunnels, la terre ou la roche."
+		saison: "hiver",
+		summary: "Labourer, creuser des tunnels, faire des fondations et constructions",
+		description: "Avec ses racines, ses pattes ou son corps fouisseur, le personnage peut creuser la terre, excaver et fragmenter les roches. Ce Don s’utilise pour labourer, creuser des tunnels, faire des fondations et constructions."
 	},
-	pistage: {
-		nom: "Pistage 🌱",
+	flair: {
+		nom: "Flair",
 		categorie: "perception",
+		saison: "printemps",
 		summary: "Traque, sentir (odorat) et goûter",
-		description: "Le personnage peut utiliser son odorat et son goût pour trouver et suivre des traces, mais aussi analyser des saveurs et odeurs. Ce Don s’utilise pour la traque, l'odorat et le goût."
+		description: "Le personnage bénéficie d’un odorat et d’un goût très développés, qui lui permettent de reconnaître et détecter des odeurs avec une grande précision. Ce Don s’utilise pour la traque, l'odorat et le goût."
 	},
 	electrorception: {
-		nom: "Électroréception 🌱",
-		categorie: "perception",	
+		nom: "Électroréception",
+		categorie: "perception",
+		saison: "printemps",
 		summary: "Perception en milieu aquatique, pour s’orienter, l'équilibre et la proprioception",
 		description: "Le personnage est capable de sentir les champs électromagnétiques. Ce Don s’utilise pour la perception en milieu aquatique, pour s’orienter, l'équilibre et la proprioception."
 	},
 	sonar: {
-		nom: "Sonar 🌱",
+		nom: "Sonar",
 		categorie: "perception",
+		saison: "printemps",
 		summary: "S’utilise pour l’ouïe et le toucher",
 		description: "Via l’écholocalisation, les vibrations du sol ou de l’eau, le personnage peut se représenter ce qui l’entoure. Ce Don s’utilise pour l’ouïe et le toucher."
 	},
 	thermoception: {
-		nom: "Thermoception 🌱",
+		nom: "Thermoception",
 		categorie: "perception",
+		saison: "printemps",
 		summary: "S'utilise pour la vue",
 		description: "Le personnage peut augmenter sa perception des couleurs pour voir dans l’infrarouge ou l’ultraviolet ou augmenter sa sensibilité à la lumière. Ce Don s'utilise pour la vue."
 	},
-	ecran: {
-		nom: "Écran ☀️",
+	charisme: {
+		nom: "Charisme",
 		categorie: "communication",
-		summary: "Fuir, mentir, bluffer, manipuler, détourner l’attention, dissimuler ses pensées, des objets ou sa présence",
-		description: "À l’aide de pollen, de parfums, d’encre ou de gaz le personnage peut subjuguer les autres. Ce Don s’utilise pour fuir, mentir, bluffer, manipuler, détourner l’attention, dissimuler ses pensées, des objets ou sa présence."	
+		saison: "ete",
+		summary: "Transmettre ou susciter des émotions",
+		description: "Par le langage corporel, des chants, changeant de couleur ou via des phéromones, le personnage communique de façon spectaculaire. Ce Don s’utilise pour transmettre ou susciter des émotions."
 	},
 	empathie: {
-		nom: "Empathie ☀️",
+		nom: "Empathie",
 		categorie: "communication",
+		saison: "ete",
 		summary: "Créer des liens, s’intégrer, comprendre les émotions et encourager à se confier",
 		description: "Le personnage utilise des phéromones, son langage corporel et ses expressions faciales pour gagner la confiance. Ce Don s’utilise pour créer des liens, s’intégrer, comprendre les émotions et encourager à se confier."
 	},
-	presence: {
-		nom: "Présence ☀️",
+	hypnose: {
+		nom: "Hypnose",
 		categorie: "communication",
-		summary: "Intimider, impressionner, effrayer et se faire obéir",
-		description: "En gonflant son corps, changeant de couleur ou via des phéromones, le personnage devient plus impressionnant et menaçant. Ce Don s’utilise pour intimider, impressionner, effrayer et se faire obéir."
+		saison: "ete",
+		summary: "endormir, mentir, bluffer ou manipuler",
+		description: "À l’aide de pollen, de parfums, d’encre ou de gaz le personnage peut subjuguer les autres. Ce Don s’utilise pour endormir, mentir, bluffer ou manipuler."
 	},
-	signaux: {
-		nom: "Signaux ☀️",
+	messages: {
+		nom: "Messages",
 		categorie: "communication",
-		summary: "Laisser des traces, communication à longue distance et spectacles",
-		description: "Le personnage communique avec des cris, chants, bioluminescence et odeurs. Ce Don s’utilise pour laisser des traces, la communication à longue distance et les spectacles."
+		saison: "ete",
+		summary: "Laisser des messages cachés et communiquer à distance",
+		description: "Le personnage utilise des cris, des phéromones, la bioluminescence ou ses racines, le personnage peut envoyer ou laisser des messages. Ce Don s’utilise pour transmettre ou laisser des messages cachés et communiquer à distance."
 	},
-	encre: {
-		nom: "Encre 🍁",
-		categorie: "fabrique",
-		summary: "?",
-		description: "?"
+	ecran: {
+		nom: "Écran",
+		categorie: "secretion",
+		saison: "automne",
+		summary: "Fuir, cacher ou se cacher, marquer et détourner l’attention",
+		description: "Le personnage peut projeter du gaz coloré et/ou odorant, de l’encre ou autre substance formant un nuage. Ce Don s’utilise pour fuir, cacher ou se cacher, marquer et détourner l’attention."	
 	},
 	venin: {
-		nom: "Venin 🍁",
-		categorie: "fabrique",
+		nom: "Venin",
+		categorie: "secretion",
+		saison: "automne",
 		summary: "S’utilise en Attaque",
-		description: "L’organisme du personnage produit des toxines mortelles. Ce Don s’utilise quand le personnage fait une Attaque."
+		description: "Le personnage produit des toxines mortelles qu’il peut injecter à ses victimes. Ce Don s’utilise quand le personnage fait une Attaque."
 	},
 	fabrique: {
-		nom: "Fabrique 🍁",
-		categorie: "fabrique",
-		summary: "?",
-		description: "?"
+		nom: "Fabrique",
+		categorie: "secretion",
+		saison: "automne",
+		summary: "Fabriquer ou réparer des objets et abris",
+		description: "Le personnage secrète en permanence une substance utile pour sa vie quotidienne, qu’il peut utiliser de multiples façons : poils, soie, bave, épines, feuilles, plumes, cire… Ce Don s’utilise quand le personnage fabrique ou répare des objets et abris."
 	},
 	reserves: {
-		nom: "Réserves 🍁",
-		categorie: "fabrique",
+		nom: "Réserves",
+		categorie: "secretion",
+		saison: "automne",
 		summary: "Efforts de longue haleine, les Expéditions et les Tâches Complexes",
 		description: "Le personnage a des réserves de nutriments sous forme de graisse, de tubercules ou autres. Ce Don s’utilise pour les efforts de longue haleine, les Expéditions et les Tâches Complexes."
 	}
